7c1359f7368a5377553500d1e3139d7284d0e74b
Igor7070 <ijijij7070@gmail.com>
2024-10-05 22:21:48 +0300

    Merge branch 'main' of https://github.com/Igor7070/convenient_website777 # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- src/main/java/com/example/parsing_vacancies/controller/AuthController.java
+++ src/main/java/com/example/parsing_vacancies/controller/AuthController.java
@@ -1,23 +1,13 @@
 package com.example.parsing_vacancies.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthController {
-
-    @Value("${GOOGLE_CLIENT_ID}")
-    private String googleClientId;
-
-    @Value("${GOOGLE_CLIENT_SECRET}")
-    private String googleClientSecret;
-
     @GetMapping("/login")
     public String login() {
         // Перенаправление на Google для аутентификации
-        System.out.println("GOOGLE_CLIENT_ID: " + googleClientId);
-        System.out.println("GOOGLE_CLIENT_SECRET: " + googleClientSecret);
         return "redirect:/oauth2/authorization/google";
     }
 
