160775ac900b8caac0f8492f8daaf5a6b79ca7bf
Igor7070 <ijijij7070@gmail.com>
2024-10-05 23:25:33 +0300

    Merge branch 'main' of https://github.com/Igor7070/convenient_website777 # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- src/main/java/com/example/parsing_vacancies/config/SecurityConfig.java
+++ src/main/java/com/example/parsing_vacancies/config/SecurityConfig.java
@@ -1,12 +1,15 @@
 package com.example.parsing_vacancies.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    /*@Bean
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
@@ -18,5 +21,5 @@ public class SecurityConfig {
                 );
 
         return http.build();
-    }*/
+    }
 }
--- src/main/java/com/example/parsing_vacancies/controller/AuthController.java
+++ src/main/java/com/example/parsing_vacancies/controller/AuthController.java
@@ -8,12 +8,14 @@ public class AuthController {
     @GetMapping("/login")
     public String login() {
         // Перенаправление на Google для аутентификации
+        System.out.println("Авторизация");
         return "redirect:/oauth2/authorization/google";
     }
 
     @GetMapping("/oauth2/callback")
     public String oauth2Callback() {
         // Обработка данных пользователя после успешной аутентификации
+        System.out.println("Успешная авторизация");
         return "redirect:/convenient_job_search"; // Перенаправление на домашнюю страницу
     }
 }
