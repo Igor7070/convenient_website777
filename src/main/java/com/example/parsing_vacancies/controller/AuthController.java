f9ba25b1a0f1f95c39c0c0b2a3585e1cf0950513
Igor7070 <ijijij7070@gmail.com>
2024-10-05 23:29:34 +0300

    Merge branch 'main' of https://github.com/Igor7070/convenient_website777 # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- src/main/java/com/example/parsing_vacancies/controller/AuthController.java
+++ src/main/java/com/example/parsing_vacancies/controller/AuthController.java
@@ -8,14 +8,14 @@ public class AuthController {
     @GetMapping("/login")
     public String login() {
         // Перенаправление на Google для аутентификации
-        System.out.println("Авторизация");
+        System.out.println("Autorization");
         return "redirect:/oauth2/authorization/google";
     }
 
     @GetMapping("/oauth2/callback")
     public String oauth2Callback() {
         // Обработка данных пользователя после успешной аутентификации
-        System.out.println("Успешная авторизация");
+        System.out.println("Success autorization");
         return "redirect:/convenient_job_search"; // Перенаправление на домашнюю страницу
     }
 }
