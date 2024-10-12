d945c7375756e17e927ebd9ddba3b4acc41127f3
Igor7070 <ijijij7070@gmail.com>
2024-10-12 21:47:26 +0300

    Merge branch 'main' of https://github.com/Igor7070/convenient_website777 # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- src/main/java/com/example/parsing_vacancies/controller/ProxyResumeController.java
+++ src/main/java/com/example/parsing_vacancies/controller/ProxyResumeController.java
@@ -208,7 +208,7 @@ public class ProxyResumeController {
 
     // Метод для проверки статуса
     private ResponseEntity<String> checkStatus(String requestId, String targetUrl) {
-        String statusUrl = targetUrl + "/" + requestId; // URL для проверки статуса
+        String statusUrl = targetUrl; // URL для проверки статуса
         return restTemplate.postForEntity(statusUrl, null, String.class);
     }
 
