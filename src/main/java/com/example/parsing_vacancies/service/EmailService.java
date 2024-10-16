66a53602b95f7df05eb92408cdde212f137b2c07
Igor7070 <ijijij7070@gmail.com>
2024-10-16 22:40:25 +0300

    Merge branch 'main' of https://github.com/Igor7070/convenient_website777 # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- src/main/java/com/example/parsing_vacancies/service/EmailService.java
+++ src/main/java/com/example/parsing_vacancies/service/EmailService.java
@@ -1,23 +1,69 @@
 package com.example.parsing_vacancies.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.gmail.Gmail;
+import com.google.api.services.gmail.model.Message;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Base64;
+import java.util.Properties;
+
 
 @Service
 public class EmailService {
 
-    @Autowired
-    private JavaMailSender mailSender;
+    public void sendSimpleMessage(String to, String subject, String body, String accessToken) {
+        try {
+            // Создание HttpRequestInitializer с использованием accessToken
+            HttpRequestInitializer requestInitializer = httpRequest -> {
+                httpRequest.getHeaders().setAuthorization("Bearer " + accessToken);
+            };
 
-    public void sendSimpleMessage(String to, String subject, String body) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("uunlimitedpossibilities@gmail.com");
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(body);
+            // Создание Gmail API клиента с использованием токена доступа
+            Gmail service = new Gmail.Builder(GoogleNetHttpTransport.newTrustedTransport(), JacksonFactory.getDefaultInstance(), requestInitializer)
+                    .setApplicationName("Unlimited Possibilities 12")
+                    .build();
 
-        mailSender.send(message);
+            // Создание сообщения
+            Properties props = new Properties();
+            Session session = Session.getInstance(props, null);
+            MimeMessage email = new MimeMessage(session);
+
+            // Указываем адрес отправителя
+            email.setFrom(new InternetAddress("uunlimitedpossibilities@gmail.com"));
+            email.addRecipient(javax.mail.Message.RecipientType.TO, new InternetAddress(to));
+            email.setSubject(subject);
+            email.setText(body);
+
+            // Отправка сообщения
+            ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+            email.writeTo(buffer);
+            byte[] bytes = buffer.toByteArray();
+            String encodedEmail = Base64.getUrlEncoder().encodeToString(bytes);
+
+            Message message = new Message();
+            message.setRaw(encodedEmail);
+
+            // Убедитесь, что адрес отправителя разрешен в настройках Gmail
+            service.users().messages().send("me", message).execute();
+        } catch (GeneralSecurityException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Security exception: " + e.getMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("IO exception: " + e.getMessage());
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Messaging exception: " + e.getMessage());
+        }
     }
 }
