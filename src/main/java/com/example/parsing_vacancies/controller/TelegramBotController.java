5bd8b94a0de273f37b4939b49d00025484ec5313
Igor7070 <ijijij7070@gmail.com>
2024-10-18 22:43:00 +0300

    Merge branch 'main' of https://github.com/Igor7070/convenient_website777 # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

---
+++ src/main/java/com/example/parsing_vacancies/controller/TelegramBotController.java
@@ -0,0 +1,10 @@
+package com.example.parsing_vacancies.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/bot")
+public class TelegramBotController {
+
+}
