d8fae38976cba3e7c389cd3a81ec6cecb53e9794
Igor7070 <ijijij7070@gmail.com>
2024-11-07 21:00:29 +0200

    Merge branch 'main' of https://github.com/Igor7070/convenient_website777 # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- src/main/java/com/example/parsing_vacancies/controller/ChatGPTController.java
+++ src/main/java/com/example/parsing_vacancies/controller/ChatGPTController.java
@@ -1,12 +1,12 @@
 package com.example.parsing_vacancies.controller;
 
+import com.example.parsing_vacancies.model.chat_ai.MessageRequest;
 import com.example.parsing_vacancies.service.OpenAIService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,5 +32,12 @@ public class ChatGPTController {
         chatMessages.add(result);
         model.addAttribute("chatMessages", chatMessages);
         return "advancedModel";
+    }
+
+    @PostMapping("/api/communicating_with_a_advanced_ai_model")
+    @ResponseBody
+    public ResponseEntity<String> generateApiCompletion(@RequestBody MessageRequest request) {
+        String result = openAIService.generateCompletion(request.getPrompt());
+        return ResponseEntity.ok(result);
     }
 }
---
+++ src/main/java/com/example/parsing_vacancies/model/chat_ai/MessageRequest.java
@@ -0,0 +1,14 @@
+package com.example.parsing_vacancies.model.chat_ai;
+
+public class MessageRequest {
+    private String prompt;
+
+    // Геттеры и сеттеры
+    public String getPrompt() {
+        return prompt;
+    }
+
+    public void setPrompt(String prompt) {
+        this.prompt = prompt;
+    }
+}
