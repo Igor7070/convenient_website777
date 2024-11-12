73686c9d76f58cc5725c1aa1870c94f4c2dd19f8
Igor7070 <ijijij7070@gmail.com>
2024-11-12 20:19:28 +0200

    Merge branch 'main' of https://github.com/Igor7070/convenient_website777 # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- src/main/java/com/example/parsing_vacancies/controller/ChatGPTController.java
+++ src/main/java/com/example/parsing_vacancies/controller/ChatGPTController.java
@@ -2,6 +2,7 @@ package com.example.parsing_vacancies.controller;
 
 import com.example.parsing_vacancies.model.chat_ai.MessageRequest;
 import com.example.parsing_vacancies.service.OpenAIService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -15,19 +16,30 @@ import java.util.List;
 public class ChatGPTController {
     @Autowired
     private OpenAIService openAIService;
-    private final List<String> chatMessages = new ArrayList<>();
+    //private final List<String> chatMessages = new ArrayList<>();
 
 
     @GetMapping("/communicating_with_a_advanced_ai_model")
-    public String chatGPT(Model model) {
+    public String chatGPT(HttpSession session, Model model) {
+        // Извлекаем историю чата из сессии или создаем новую, если ее нет
+        List<String> chatMessages = (List<String>) session.getAttribute("chatMessages");
+        if (chatMessages == null) {
+            chatMessages = new ArrayList<>();
+            session.setAttribute("chatMessages", chatMessages);
+        }
+
         model.addAttribute("title", "Продвинутая модель");
         model.addAttribute("chatMessages", chatMessages);
         return "advancedModel";
     }
 
     @PostMapping("/communicating_with_a_advanced_ai_model")
-    public String generateCompletion(@RequestParam(name = "prompt") String prompt, Model model) {
+    public String generateCompletion(@RequestParam(name = "prompt") String prompt,
+                                     HttpSession session, Model model) {
         String result = openAIService.generateCompletion(prompt);
+
+        // Извлекаем историю чата из сессии
+        List<String> chatMessages = (List<String>) session.getAttribute("chatMessages");
         chatMessages.add(prompt);
         chatMessages.add(result);
         model.addAttribute("chatMessages", chatMessages);
