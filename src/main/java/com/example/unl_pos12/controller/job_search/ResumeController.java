0ed8ce9751e24fbdfe39af99211f07f3927c8fa1
Igor7070 <ijijij7070@gmail.com>
2024-11-22 21:06:55 +0200

    Merge branch 'main' of https://github.com/Igor7070/convenient_website777 # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- src/main/java/com/example/unl_pos12/controller/job_search/ResumeController.java
+++ src/main/java/com/example/unl_pos12/controller/job_search/ResumeController.java
@@ -491,7 +491,7 @@ public class ResumeController {
 
         if (files != null) {
             long currentTime = System.currentTimeMillis();
-            long expirationTime = TimeUnit.MINUTES.toMillis(21); // 20 минут
+            long expirationTime = TimeUnit.MINUTES.toMillis(23); // 20 минут
 
             for (File file : files) {
                 if (!file.getName().equals("README.md") && currentTime - file.lastModified() > expirationTime) {
