0199d1c285ff9f72cea863ec48f79f3d4345fd12
Igor7070 <ijijij7070@gmail.com>
2024-12-13 21:46:47 +0200

    Merge branch 'main' of https://github.com/Igor7070/convenient_website777 # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- src/main/java/com/example/unl_pos12/controller/messenger/ProxyController.java
+++ src/main/java/com/example/unl_pos12/controller/messenger/ProxyController.java
@@ -2,12 +2,16 @@ package com.example.unl_pos12.controller.messenger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/messenger")
 public class ProxyController {
+
     private final RestTemplate restTemplate;
 
     @Autowired
@@ -16,14 +20,12 @@ public class ProxyController {
     }
 
     @GetMapping("/**")
-    @ResponseBody
     public ResponseEntity<String> proxyResources(@PathVariable(value = "path") String path) {
         String resourceUrl = "https://igor7070.github.io/Messenger/" + path;
         return restTemplate.getForEntity(resourceUrl, String.class);
     }
 
     @GetMapping
-    @ResponseBody
     public ResponseEntity<String> proxyHome() {
         String resourceUrl = "https://igor7070.github.io/Messenger/";
         return restTemplate.getForEntity(resourceUrl, String.class);
