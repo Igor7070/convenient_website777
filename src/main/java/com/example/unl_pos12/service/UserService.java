d4048f25b0d738799c922f2cd1ae041d350eb94d
Igor7070 <ijijij7070@gmail.com>
2024-12-16 14:16:55 +0200

    Merge branch 'main' of https://github.com/Igor7070/convenient_website777 # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- src/main/java/com/example/unl_pos12/service/UserService.java
+++ src/main/java/com/example/unl_pos12/service/UserService.java
@@ -13,23 +13,31 @@ public class UserService {
     private UserRepository userRepository;
 
     public User createUser(User user) {
+        String errorMessage = "";
         // Проверка на существование пользователя
         if (userRepository.findByUsername(user.getUsername()).isPresent()) {
-            throw new RuntimeException("User already exists");
+            errorMessage = "User already exists";
+            System.out.println(errorMessage);
+            throw new RuntimeException(errorMessage);
         }
         // Проверка на валидность пароля
         if (!isValidPassword(user.getPassword())) {
-            throw new RuntimeException("Password must be at least 9 characters long, contain letters and numbers");
+            errorMessage = "Password must be at least 9 characters long, contain letters and numbers";
+            System.out.println(errorMessage);
+            throw new RuntimeException(errorMessage);
         }
         return userRepository.save(user);
     }
 
     public User loginUser(String username, String password) {
+        String errorMessage = "";
         Optional<User> user = userRepository.findByUsername(username);
         if (user.isPresent() && user.get().getPassword().equals(password)) {
             return user.get();
         } else {
-            throw new RuntimeException("Invalid username or password");
+            errorMessage = "Invalid username or password";
+            System.out.println(errorMessage);
+            throw new RuntimeException(errorMessage);
         }
     }
 
