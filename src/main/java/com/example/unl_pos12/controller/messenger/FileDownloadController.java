44822ca39a5c03721869d78af1b2a21263e8a491
Igor7070 <ijijij7070@gmail.com>
2024-12-31 16:14:51 +0200

    Merge branch 'main' of https://github.com/Igor7070/convenient_website777 # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- src/main/java/com/example/unl_pos12/controller/messenger/FileDownloadController.java
+++ src/main/java/com/example/unl_pos12/controller/messenger/FileDownloadController.java
@@ -10,7 +10,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -20,7 +23,9 @@ public class FileDownloadController {
     @GetMapping("/download/{filename:.+}")
     public ResponseEntity<Resource> downloadFile(@PathVariable String filename) {
         try {
-            Path filePath = Paths.get("uploads").resolve(filename);
+            // Декодируем имя файла
+            String decodedFilename = URLDecoder.decode(filename, StandardCharsets.UTF_8.toString());
+            Path filePath = Paths.get("uploads").resolve(decodedFilename);
             Resource resource = new UrlResource(filePath.toUri());
 
             if (resource.exists() && resource.isReadable()) {
@@ -31,6 +36,9 @@ public class FileDownloadController {
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
             }
         } catch (MalformedURLException e) {
+            e.printStackTrace(); // Логирование ошибки
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        } catch (UnsupportedEncodingException e) {
             e.printStackTrace(); // Логирование ошибки
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
