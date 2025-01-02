a44a6fe31f5bfb578f744312525a50394ce0053e
Igor7070 <ijijij7070@gmail.com>
2025-01-02 22:35:34 +0200

    Merge branch 'main' of https://github.com/Igor7070/convenient_website777 # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- src/main/java/com/example/unl_pos12/model/messenger/Chat.java
+++ src/main/java/com/example/unl_pos12/model/messenger/Chat.java
@@ -11,7 +11,7 @@ public class Chat {
     private Long id;
     private String name;
 
-    @OneToMany(mappedBy = "chat", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "chat", fetch = FetchType.EAGER)
     private List<Message> messages;
 
     public Long getId() {
