50b02047df00427ea20f4d8ad4224388fa9d9b7e
Igor7070 <ijijij7070@gmail.com>
2025-01-03 14:33:04 +0200

    Merge branch 'main' of https://github.com/Igor7070/convenient_website777 # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- src/main/java/com/example/unl_pos12/controller/messenger/WebSocketMessageController.java
+++ src/main/java/com/example/unl_pos12/controller/messenger/WebSocketMessageController.java
@@ -23,6 +23,8 @@ public class WebSocketMessageController {
         System.out.println("Received message: " + message.getContent() +
                 " from user: " + message.getSender().getUsername());
         System.out.println("Message from chat: " + message.getChat().getName());
+        System.out.println("Message from chat: " + (message.getChat() != null
+                ? message.getChat().getId() : "null"));
 
         // Убедитесь, что chatId установлен
         if (message.getChat() != null && message.getChat().getId() != null) {
