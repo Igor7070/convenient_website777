6b74ca0ae42be37c449b0547068c47cd3dab1154
Igor7070 <ijijij7070@gmail.com>
2025-01-10 20:47:31 +0200

    Merge branch 'main' of https://github.com/Igor7070/convenient_website777 # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- src/main/java/com/example/unl_pos12/config/messenger/WebConfigAvatars.java
+++ src/main/java/com/example/unl_pos12/config/messenger/WebConfigAvatars.java
@@ -1,12 +1,11 @@
 package com.example.unl_pos12.config.messenger;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
+//@EnableWebMvc
 public class WebConfigAvatars implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
