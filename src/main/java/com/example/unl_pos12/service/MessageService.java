28c1cf35a7913b45561701339e1773063f523467
Igor7070 <ijijij7070@gmail.com>
2025-07-04 21:41:59 +0300

    Merge branch 'main' of https://github.com/Igor7070/convenient_website777 # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- src/main/java/com/example/unl_pos12/service/MessageService.java
+++ src/main/java/com/example/unl_pos12/service/MessageService.java
@@ -92,7 +92,7 @@ public class MessageService {
             throw new RuntimeException("File size exceeds 100 MB");
         }
 
-        String uploadDir = "Uploads/";
+        String uploadDir = "uploads/";
         File directory = new File(uploadDir);
         if (!directory.exists()) {
             directory.mkdirs();
