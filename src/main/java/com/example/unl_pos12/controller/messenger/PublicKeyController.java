08947f1588d801cb229055436529db38da3bc035
Igor7070 <ijijij7070@gmail.com>
2025-10-14 14:11:57 +0300

    Merge branch 'main' of https://github.com/Igor7070/convenient_website777 # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- src/main/java/com/example/unl_pos12/controller/messenger/PublicKeyController.java
+++ src/main/java/com/example/unl_pos12/controller/messenger/PublicKeyController.java
@@ -4,6 +4,7 @@ import com.example.unl_pos12.model.messenger.PublicKey;
 import com.example.unl_pos12.repo.PublicKeyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.*;
 public class PublicKeyController {
     @Autowired
     private PublicKeyRepository publicKeyRepository;
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate; // [ADD] Внедрение SimpMessagingTemplate
 
     @GetMapping("/{userId}")
     public ResponseEntity<PublicKey> getPublicKey(@PathVariable Long userId) {
@@ -26,7 +29,15 @@ public class PublicKeyController {
     }
 
     @PostMapping
-    public PublicKey savePublicKey(@RequestBody PublicKey publicKey) {
-        return publicKeyRepository.save(publicKey);
+    public ResponseEntity<PublicKey> savePublicKey(@RequestBody PublicKey publicKey) { // [CHANGE] Изменён возвращаемый тип на ResponseEntity<PublicKey>
+        System.out.println("Received POST /api/public_keys for userId: " + publicKey.getUserId() + ", key: " + publicKey.getPublicKey());
+        PublicKey savedPublicKey = publicKeyRepository.save(publicKey);
+        System.out.println("Saved public key for userId: " + savedPublicKey.getUserId() + ", key: " + savedPublicKey.getPublicKey());
+
+        // [ADD] Отправка уведомления через WebSocket
+        messagingTemplate.convertAndSend("/topic/publicKey/" + savedPublicKey.getUserId(), savedPublicKey);
+        System.out.println("Sent WebSocket notification to /topic/publicKey/" + savedPublicKey.getUserId() + ": " + savedPublicKey.getPublicKey());
+
+        return ResponseEntity.ok(savedPublicKey); // [CHANGE] Возвращаем ResponseEntity
     }
 }
