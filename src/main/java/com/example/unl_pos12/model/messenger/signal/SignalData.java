800462e26e357bfa00b4741552ae7c6bec36939f
Igor7070 <ijijij7070@gmail.com>
2025-11-22 19:27:12 +0200

    Merge branch 'main' of https://github.com/Igor7070/convenient_website777 # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- src/main/java/com/example/unl_pos12/model/messenger/signal/SignalData.java
+++ src/main/java/com/example/unl_pos12/model/messenger/signal/SignalData.java
@@ -4,6 +4,8 @@ public class SignalData {
     private String type; // Тип сигнала (offer, answer, candidate)
     private String sdp; // SDP-строка
     private IceCandidate iceCandidate;
+    private String signature;     // подпись SDP (base64)
+    private String publicKey;     // публичный ключ подписи Ed25519 (base64)
 
     public String getType() {
         return type;
@@ -29,12 +31,30 @@ public class SignalData {
         this.iceCandidate = iceCandidate;
     }
 
+    public String getSignature() {
+        return signature;
+    }
+
+    public void setSignature(String signature) {
+        this.signature = signature;
+    }
+
+    public String getPublicKey() {
+        return publicKey;
+    }
+
+    public void setPublicKey(String publicKey) {
+        this.publicKey = publicKey;
+    }
+
     @Override
     public String toString() {
         return "SignalData{" +
                 "type='" + type + '\'' +
                 ", sdp='" + sdp + '\'' +
                 ", iceCandidate=" + iceCandidate +
+                ", signature=" + signature +
+                ", publicKey=" + publicKey +
                 '}';
     }
 }
